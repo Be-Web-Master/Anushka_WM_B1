a49e043aa6a3b6e8a484c024b8c8b06631c979cd
Anushka1979 <anushkavishwari00@gmail.com>
2023-08-21 11:57:51 +0530

    loop practice question

--- day19/question26.js
+++ day19/question26.js
@@ -1,8 +1,21 @@
-//26. Write a program to swap the first and last digits of a number.
+// 26.  Write a program to swap the first and last digits of a number.
 
-function swapFirstAndLastDigit(num){
-    let newNum = ""+num,
-    chunck=newNum.split([]);
-    
+
+function swapNumbers(num){
+    let newNum="", temp="";
+    for(let i=0;i<num.length;i++){
+        if(i === 0){
+            temp = num[i];
+        } 
+        else if(i === num.length-1){
+            newNum = newNum + temp;
+            newNum = num[i] + newNum;
+            
+        }
+        else {
+             newNum = newNum + num[i]
+        }
+    }
+console.log(newNum);
 }
-swapFirstAndLastDigit(123)
+swapNumbers("123")
