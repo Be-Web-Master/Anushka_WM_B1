0e9feb3a540539a3907c1666402f084e52bff32f
Anushka1979 <anushkavishwari00@gmail.com>
2023-08-26 23:51:39 +0530

    create table -> addColumn , addRow and clear

--- script.js
+++ script.js
@@ -1,152 +1,127 @@
 const thead = document.getElementById('thead')
 const tbody = document.getElementById('tbody')
-const table = {
-    header :[{
-        element: 'th',
-        columnName:'column A',
-        order:1,
-        sort: undefined
-    },{
-        element: 'th',
-        columnName:'column B',
-        order:2,
-        sort: undefined
-    },{
-        element: 'th',
-        columnName:'column C',
-        order:3,
-        sort: undefined
-    }
-    ],
-    body:[{
-        element: 'tr',
-        rowOrder: 1,
-        columnElements:[{
-            elementType: 'td',
-            columnName:'column A',
-            rowOrder:1,
-            value:'row 1'
-        },{
-            elementType: 'td',
-            columnName:'column A',
-            rowOrder:1,
-            value:'row 2'
-        },{
-            elementType: 'td',
-            columnName:'column A',
-            rowOrder:1,
-            value:'row 3'
-        } ]
-    },
-    {
-        element: 'tr',
-        rowOrder: 2,
-        columnElements:[{
-            elementType: 'td',
-            columnName:'column B',
-            rowOrder:2,
-            value:'row 1'
-        },{
-            elementType: 'td',
-            columnName:'column B',
-            rowOrder:2,
-            value:'row 2'
-        },{
-            elementType: 'td',
-            columnName:'column B',
-            rowOrder:2,
-            value:'row 3'
-        } ]
-    },
-    {
-        element: 'tr',
-        rowOrder: 3,
-        columnElements:[{
-            elementType: 'td',
-            columnName:'column C',
-            rowOrder:3,
-            value:'row 1'
-        },{
-            elementType: 'td',
-            columnName:'column C',
-            rowOrder:3,
-            value:'row 2'
-        },{
-            elementType: 'td',
-            columnName:'column C',
-            rowOrder:3,
-            value:'row 3'
-        } ]
-    }]
+const LOCAL_STORAGE_TABLE_KEY = 'tableSateData'
+let tableState = {
+    header :[],
+    body:[]
+}
+tbody.addEventListener('keyup',(event)=>{
+    console.log(event.target)
+})
+thead.addEventListener('keyup',(event)=>{
+    console.log(event.target)
+})
+// to store local data in local storage
+if(localStorage.getItem
+    (LOCAL_STORAGE_TABLE_KEY)){
+        //agr data hoga to override kr dega tableState m
+    tableState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TABLE_KEY));
+}else{
+    //jo apn data denge wo save kr dega
+    localStorage.setItem(LOCAL_STORAGE_TABLE_KEY, JSON.stringify(tableState));
 }
 
 const columnAddButton = document.getElementById('columnBtn')
-columnAddButton.onclick = addColumnHead
+columnAddButton.onclick = addNewColumn
 const rowAddButton = document.getElementById('rowBtn')
-rowAddButton.onclick = addRowData
+rowAddButton.onclick = addNewRow
+const clearTableButton = document.getElementById('clearBtn')
+clearTableButton.onclick = clearTableData
 
 function createTableUI(){
-    
-    for (const data of table.header) {
-       const th = document.createElement(data.element)
-       th.innerText = data.columnName
+    thead.innerHTML = ''
+    tbody.innerHTML = ''
+
+    for (const columnText of tableState.header) {
+       const th = document.createElement(columnText.elementType)
+       const columnElement = createInput(columnText.columnNumber)
+       columnElement.value = columnText.columnName
+       th.append(columnElement)
        thead.append(th)
     }
-    for (const data of table.body) {
-        // console.log(data.columnElements[0].elementType)
-        const tr = document.createElement(data.element)
-        for (const rowElement of data.columnElements) {
+    for (const row of tableState.body) {
+        const tr = document.createElement(row.elementType)
+        for (const rowElement of row.cellDetails) {
             const td = document.createElement(rowElement.elementType)
-            td.innerText = rowElement.value 
+            // console.log(rowElement)
+            const elementRowInput = createTextArea(rowElement.rowNumber,rowElement.columnNumber)
+            elementRowInput.value = rowElement.cellValue
+            td.append(elementRowInput)
             tr.append(td)   
         }
         tbody.append(tr)
     }
+    updateTableData()
 }
 createTableUI()
 
-
-function addColumnHead(){
-    const th = document.createElement('th')
-    thead.appendChild(th)
-    console.log(thead.innerHTML)
-    th.innerText = `Column`
-    th.style.padding = '10px'
-    th.style.border = '1px solid #000'
-    if (tbody.childElementCount) {
-        for (const el of tbody.children) {
-            console.log({el})
-            const td = document.createElement('td')
-            el.appendChild(td)
-            td.append(createInput())
-            td.style.padding = '10px'
-            td.style.border = '1px solid #000'
-        }
+//to create column
+function addNewColumn(){
+    const headerObject= {
+        elementType: 'th',
+        columnName:'',
+        columnNumber: tableState.header.length,
+        sort: undefined
     }
+    tableState.header.push(headerObject);
+    tableState.body.forEach((rowObj, idx)=>{
+        rowObj.cellDetails.push({
+            elementType : 'td',
+            columnNumber:'',
+            rowNumber: idx+1,
+            cellValue : ''
+        })
+    })
+    createTableUI(tableState);
 
 }
-
-function addRowData(){
-    const tr = document.createElement('tr')
-    tbody.appendChild(tr)
-    for(i=0;i<thead.children.length ; i++){    
-        const td = document.createElement('td')
-        tr.appendChild(td)
-        td.append(createInput())
-        td.style.padding = '10px'
-        td.style.border = '1px solid #000'
-
+//to create row
+function addNewRow(){
+    const bodyObject = {
+        elementType : 'tr',
+        rowNumber : tableState.body.length+1,
+        cellDetails:[]
     }
+    tableState.header.forEach((headerElement)=>{
+        // console.log(headerElement)
+        bodyObject.cellDetails.push(
+            {
+                elementType : 'td',
+                columnNumber:headerElement.columnNumber +1,
+                rowNumber : tableState.body.length+1,
+                cellValue : ''
+            }
+        )
+    })
+    tableState.body.push(bodyObject)
+    createTableUI(tableState)
+    
 }
-
-function createInput(){
+// to create new input
+function createInput(columnNumber){
     const input = Object.assign(document.createElement('input'),
     {
+        id : `0-${columnNumber}`,
         type: 'text',
-        placeholder: 'Enter your text'
+        placeholder: 'Column name'
     })
     return input
 }
 
+function createTextArea(rowNumber,columnNumber){
+    const rowTextArea = Object.assign(document.createElement('textarea'),
+    {   
+        id:`${rowNumber}-${columnNumber}`,
+        placeholder: 'Row data'
+    })
+    return rowTextArea
+}
 
+function updateTableData(){
+    localStorage.setItem(LOCAL_STORAGE_TABLE_KEY, JSON.stringify(tableState));
+}
+
+function clearTableData(){
+    localStorage.clear()
+}
 
