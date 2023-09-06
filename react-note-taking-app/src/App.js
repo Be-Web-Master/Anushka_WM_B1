7357696c0badf09989b006dce86feb1217aab7a5
Anushka1979 <anushkavishwari00@gmail.com>
2023-09-07 02:58:30 +0530

    react task incomplete edit feature

---
+++ react-note-taking-app/src/App.js
@@ -0,0 +1,91 @@
+import './App.css'
+import { useState } from 'react'
+function NoteTakingApp() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [notes, setNotes] = useState([])
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const buttonSubmit = () => {
+    const newNotes = { title, description }
+    setNotes([...notes, newNotes])
+    setTitle('')
+    setDescription('')
+    console.log(newNotes)
+  }
+  function deleteNote(id) {
+    setNotes((data) => data.filter((elem, i) => i !== id))
+  }
+  function editNote(id){
+      setTitle(notes[id].title)
+      setDescription(notes[id].description)
+      setShowEditModal(true)
+  }
+  return (<>
+  <h2>Notes Modal</h2>
+    <form className='form' onSubmit={(event) => {
+      event.preventDefault()
+    }}>
+      <input
+        type="text"
+        id="noteTitle"
+        name="noteTitle"
+        placeholder='Title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <textarea
+        id="noteDescription"
+        name="noteDescription"
+        placeholder='Description'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        rows="4"
+        cols="50"
+        required
+      ></textarea>
+      <button className='submitButton' type='submit' onClick={buttonSubmit} >Submit</button>
+    </form>
+    <hr />
+
+    <div className='Note'>
+      {notes.map((notes, i) => {
+        return <div className='card' key={i}>
+          <strong>{notes.title}
+            <span className="deleteNote" onClick={() => deleteNote(i)}>❌</span>
+            <span className='editNote' onClick={() => editNote(i)}>🖊</span></strong>
+          {notes.description}
+        </div>
+      })}
+    </div>
+    {showEditModal && (<div className='modal'>
+      <h2>Edit Modal</h2>
+      <form className='form' onSubmit={(event) => {
+        event.preventDefault()
+      }}>
+        <input
+          type="text"
+          id="noteTitle"
+          name="noteTitle"
+          placeholder='Title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <textarea
+          id="noteDescription"
+          name="noteDescription"
+          placeholder='Description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          rows="4"
+          cols="50"
+          required
+        ></textarea>
+        <button className='submitButton' type='submit' onClick={buttonSubmit} >Submit</button>
+      </form>
+    </div>)}
+  </>)
+}
+export default NoteTakingApp
